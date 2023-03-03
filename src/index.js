179e13927e8a3c902149acb2c280a7a4d57bd1bd
胡星 <304602555@qq.com>
2023-03-03 10:55:10 +0800

    登录页美化

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './router/index';
 import reportWebVitals from './reportWebVitals';
+import { ConfigProvider } from 'antd';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ConfigProvider theme={{
+    token: {
+      colorPrimary: '#189F92',
+    }
+  }}>
     <App />
-  </React.StrictMode>
+  </ConfigProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import { Button, Form, Input } from 'antd';
+import bg from '../../static/img/bg.png'
+import shadow from '../../static/img/aiwrap.png'
 
 const App = () => {
   const onFinish = (values) => {
@@ -11,67 +13,89 @@ const App = () => {
     <>
         <div style={{
             backgroundColor: '#2d3a4b',
+            backgroundImage: `url(${bg})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: '100vw 100vh',
             height: '100vh'
         }}>
             <div style={{
-                width: 400,
-                height: 400,
+                width: 420,
+                height: 420,
                 margin: 'auto',
                 position: 'fixed',
                 top: 0,
                 left: 0,
                 bottom: 0,
                 right: 0,
+                borderRadius: 4,
+                backgroundColor: '#FFFFFF'
             }}>
-                <h3 style={{color: '#FFFFFF',textAlign: 'center',fontSize: 26}}>系统登录</h3>
-                <Form
-                    name="normal_login"
-                    className="login-form"
-                    initialValues={{
-                        remember: true,
-                    }}
-                    onFinish={onFinish}
-                    >
-                    <Form.Item
-                        name="username"
-                        rules={[
-                        {
-                            required: true,
-                            message: '请输入账号',
-                        },
-                        ]}
-                    >
-                        <Input size="large" prefix={<UserOutlined className="site-form-item-icon" />} placeholder="账号" />
-                    </Form.Item>
-                    <Form.Item
-                        name="password"
-                        rules={[
-                        {
-                            required: true,
-                            message: '请输入密码',
-                        },
-                        ]}
-                    >
-                        <Input
-                            prefix={<LockOutlined className="site-form-item-icon" />}
-                            type="password"
-                            placeholder="密码"
-                            size="large"
-                        />
-                    </Form.Item>
-                    {/* <Form.Item>
-                        <a className="login-form-forgot" href="">
-                        Forgot password
-                        </a>
-                    </Form.Item> */}
-                    <Form.Item>
-                        <Button style={{
-                            width: '100%'
-                        }} size="large" type="primary" htmlType="submit" className="login-form-button">
-                            登录
-                        </Button>
-                    </Form.Item>
-                </Form>
+                <div style={{width: '80%', margin: '0 auto', position: 'relative'}}>
+                    <img src={shadow} style={{
+                        position: 'absolute',
+                        top: 58,
+                        zIndex: 1,
+                        left: -60
+                    }} alt="" />
+                    <div style={{
+                        margin: "55px 0 0 -58px",
+                        padding: "18px 10px 18px 60px",
+                        background: "#189F92",
+                        position: "relative",
+                        color: "#fff",
+                        fontSize: 16
+                    }}>系统登录</div>
+                    <div style={{height: 55}}></div>
+                    <Form
+                        autoComplete="off"
+                        name="normal_login"
+                        className="login-form"
+                        initialValues={{
+                            remember: true,
+                        }}
+                        onFinish={onFinish}
+                        >
+                        <Form.Item
+                            name="username"
+                            rules={[
+                            {
+                                required: true,
+                                message: '请输入账号',
+                            },
+                            ]}
+                        >
+                            <Input size="large" prefix={<UserOutlined className="site-form-item-icon" />} placeholder="账号" />
+                        </Form.Item>
+                        <Form.Item
+                            name="password"
+                            rules={[
+                            {
+                                required: true,
+                                message: '请输入密码',
+                            },
+                            ]}
+                        >
+                            <Input
+                                prefix={<LockOutlined className="site-form-item-icon" />}
+                                type="password"
+                                placeholder="密码"
+                                size="large"
+                            />
+                        </Form.Item>
+                        {/* <Form.Item>
+                            <a className="login-form-forgot" href="">
+                            Forgot password
+                            </a>
+                        </Form.Item> */}
+                        <Form.Item>
+                            <Button style={{
+                                width: '100%'
+                            }} size="large" type="primary" htmlType="submit" className="login-form-button">
+                                登录
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </div>
             </div>
         </div>
         
