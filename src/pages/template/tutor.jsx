4e4b16a42bcc7663d41f6c04651c662d575f2eda
胡星 <304602555@qq.com>
2024-04-19 11:19:13 +0800

    v1.0.4

--- src/pages/template/tutor.jsx
+++ src/pages/template/tutor.jsx
@@ -1,7 +1,7 @@
-import React,{ useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import http from '../../lib/http'
 import * as apis from '../../lib/api'
-import { Table,Input, message, Button, Modal,Image, Form, Select, InputNumber, Tooltip,Popconfirm } from 'antd';
+import { Table, Input, message, Button, Modal, Image, Form, Select, InputNumber, Tooltip, Popconfirm } from 'antd';
 import { cloneDeep } from "lodash"
 import Upload from '../../components/upload'
 import axios from 'axios'
@@ -31,7 +31,7 @@ const Index = () => {
         {
             value: "fable",
             label: "fable"
-        },{
+        }, {
             value: "nova",
             label: "nova"
         },
@@ -40,20 +40,24 @@ const Index = () => {
             label: "shimmer"
         },
         {
-            value: "SSML_VOICE_GENDER_UNSPECIFIED",
-            label: "谷歌-SSML_VOICE_GENDER_UNSPECIFIED"
+            value: "en-US-Neural2-A",
+            label: "谷歌-en-US-Neural2-A"
         },
         {
-            value: "MALE",
-            label: "谷歌-MALE"
+            value: "en-US-Neural2-C",
+            label: "谷歌-en-US-Neural2-C"
         },
         {
-            value: "FEMALE",
-            label: "谷歌-FEMALE"
+            value: "en-US-Neural2-D",
+            label: "谷歌-en-US-Neural2-D"
         },
         {
-            value: "NEUTRAL",
-            label: "谷歌-NEUTRAL"
+            value: "en-US-Neural2-E",
+            label: "谷歌-en-US-Neural2-E"
+        },
+        {
+            value: "en-US-Neural2-E",
+            label: "谷歌-en-US-Neural2-E"
         }
     ])
 
@@ -62,22 +66,22 @@ const Index = () => {
             let data = await http.get(`${apis.TEMPLATE_LIST}?tp=role`)
             setResponse(data)
         } catch (error) {
-            
+
         }
     }
 
     const updateContent = async (obj) => {
         try {
-            await http.post(apis.TEMPLATE_UPDATE,obj)
+            await http.post(apis.TEMPLATE_UPDATE, obj)
         } catch (error) {
-            
+
         }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         setLoading(true)
 
-        loadData().then(()=>{
+        loadData().then(() => {
             setLoading(false)
         })
 
@@ -85,13 +89,13 @@ const Index = () => {
             setUpdate(new Date().getTime())
         }
 
-        window.addEventListener("resize",onresize)
+        window.addEventListener("resize", onresize)
         return () => {
-            window.removeEventListener("resize",onresize)
+            window.removeEventListener("resize", onresize)
         }
 
         // eslint-disable-next-line
-    },[])
+    }, [])
 
     const downLoad = async (item) => {
         setLoading(true)
@@ -104,23 +108,23 @@ const Index = () => {
             a.style.display = "none"
             a.click()
         } catch (error) {
-            
+
         }
         setLoading(false)
     }
 
-    const deleteKnowledge = async (item,item2) => {
+    const deleteKnowledge = async (item, item2) => {
         setLoading(true)
         try {
             await http.post(`${apis.KNOWLEDGE_DELETE}?knowledge_id=${item.id}&brain_id=${item2.brain_id}`)
             await loadData()
             message.success("删除成功")
         } catch (error) {
-            
+
         }
         setLoading(false)
     }
-    
+
 
     const columns = [
         {
@@ -140,7 +144,7 @@ const Index = () => {
             dataIndex: 'role_avatar',
             width: 120,
             render: (v) => (
-                <Image src={process.env.REACT_APP_OSS_URL+"/"+v} style={{width: 80,height:80}}></Image>
+                <Image src={process.env.REACT_APP_OSS_URL + "/" + v} style={{ width: 80, height: 80 }}></Image>
             )
         },
         {
@@ -157,10 +161,10 @@ const Index = () => {
             title: '知识库文件',
             dataIndex: 'knowledges',
             width: 220,
-            render: (v,r)=>(
+            render: (v, r) => (
                 v && <div>
                     {
-                        v.map((v2,k) => (
+                        v.map((v2, k) => (
                             <div key={k} style={{
                                 display: "flex",
                                 alignItems: "center",
@@ -184,7 +188,7 @@ const Index = () => {
                                         title="确定删除吗?"
                                         // description="确定删除吗?"
                                         onConfirm={() => {
-                                            deleteKnowledge(v2,r)
+                                            deleteKnowledge(v2, r)
                                         }}
                                         // onCancel={cancel}
                                         okText="Yes"
@@ -193,7 +197,7 @@ const Index = () => {
                                         <Button danger type="link" icon={<DeleteOutlined />}></Button>
                                     </Popconfirm>
 
-                                    
+
                                 </div>
                             </div>
                         ))
@@ -205,7 +209,7 @@ const Index = () => {
             title: '系统模板',
             dataIndex: 'system_prompt',
             width: 500,
-            render: (v,r,dx) => (
+            render: (v, r, dx) => (
                 <div>
                     <Input.TextArea disabled value={v} onChange={e => {
                         let cpList = cloneDeep(response)
@@ -219,7 +223,7 @@ const Index = () => {
             title: '用户模板',
             dataIndex: 'user_prompt',
             width: 500,
-            render: (v,r,dx) => (
+            render: (v, r, dx) => (
                 <div>
                     <Input.TextArea disabled onChange={e => {
                         let cpList = cloneDeep(response)
@@ -234,14 +238,14 @@ const Index = () => {
             dataIndex: 'action',
             fixed: 'right',
             width: 120,
-            render: (v,r) => (
+            render: (v, r) => (
                 <>
                     <Button type="link" onClick={() => {
                         setOpen(true)
                         console.log(r)
-                        let cpR =cloneDeep(r)
-                        if(cpR.role_avatar){
-                            cpR.avatar = [{path: cpR.role_avatar,thumbUrl: process.env.REACT_APP_OSS_URL+"/"+cpR.role_avatar}]
+                        let cpR = cloneDeep(r)
+                        if (cpR.role_avatar) {
+                            cpR.avatar = [{ path: cpR.role_avatar, thumbUrl: process.env.REACT_APP_OSS_URL + "/" + cpR.role_avatar }]
                         }
                         form.resetFields()
                         form.setFieldsValue(cpR)
@@ -256,7 +260,7 @@ const Index = () => {
     ];
 
     const fileChange = async e => {
-        if(e.target.files && e.target.files.length !== 0){
+        if (e.target.files && e.target.files.length !== 0) {
             let file = e.target.files[0]
             setLoading(true)
             const formData = new FormData();
@@ -268,13 +272,13 @@ const Index = () => {
                         'Authorization': localStorage.getItem('token')
                     }
                 });
-                if(response.data.code !== 200){
+                if (response.data.code !== 200) {
                     message.error(response.data.msg)
-                }else{
+                } else {
                     await loadData()
                     message.success("成功")
                 }
-                
+
             } catch (error) {
                 message.error("失败")
             }
@@ -288,56 +292,56 @@ const Index = () => {
 
     return (
         <div>
-            <input type="file" style={{display: "none"}} ref={uploadRef} accept=".xls,.doc,.txt,.pdf,.csv" onChange={fileChange} />
-            <span style={{display: "none"}}>{update}</span> 
+            <input type="file" style={{ display: "none" }} ref={uploadRef} accept=".xls,.doc,.txt,.pdf,.csv" onChange={fileChange} />
+            <span style={{ display: "none" }}>{update}</span>
             <Modal
                 title="新建/更新"
                 open={open}
                 width={'60%'}
                 forceRender
-                onCancel={()=>{
+                onCancel={() => {
                     setOpen(false)
                 }}
-                okButtonProps={{loading}}
+                okButtonProps={{ loading }}
                 onOk={async () => {
                     setLoading(true)
                     try {
                         let data = await form.validateFields()
                         let cpData = cloneDeep(data)
-                        if(cpData.avatar && cpData.avatar.length !== 0){
+                        if (cpData.avatar && cpData.avatar.length !== 0) {
                             cpData.role_avatar = cpData.avatar[0].path
                         }
                         await updateContent(cpData)
                         await loadData()
                         message.success("成功")
                     } catch (error) {
-                        
+
                     }
                     setLoading(false)
                     setOpen(false)
                 }}
             >
-                <div style={{maxHeight: 500, overflow: "auto"}}>
-                    <Form form={form} labelCol={{span: 4}}>
+                <div style={{ maxHeight: 500, overflow: "auto" }}>
+                    <Form form={form} labelCol={{ span: 4 }}>
                         <Form.Item name="id" hidden>
                             <InputNumber />
                         </Form.Item>
                         <Form.Item name="avatar" label="头像">
                             <Upload />
                         </Form.Item>
-                        <Form.Item name="role_name" rules={[{required: true}]} label="名字">
+                        <Form.Item name="role_name" rules={[{ required: true }]} label="名字">
                             <Input placeholder="请输入" />
                         </Form.Item>
-                        <Form.Item name="role_description" rules={[{required: true}]} label="描述">
+                        <Form.Item name="role_description" rules={[{ required: true }]} label="描述">
                             <Input.TextArea placeholder="请输入" rows={3} />
                         </Form.Item>
-                        <Form.Item name="tts_role" rules={[{required: true}]} label="tts角色">
+                        <Form.Item name="tts_role" rules={[{ required: true }]} label="tts角色">
                             <Select options={ttsRoles.current} placeholder="请选择"></Select>
                         </Form.Item>
-                        <Form.Item name="system_prompt" rules={[{required: true}]} label="系统提示词">
+                        <Form.Item name="system_prompt" rules={[{ required: true }]} label="系统提示词">
                             <Input.TextArea placeholder="请输入" rows={10} />
                         </Form.Item>
-                        <Form.Item name="user_prompt" rules={[{required: true}]} label="用户提示词">
+                        <Form.Item name="user_prompt" rules={[{ required: true }]} label="用户提示词">
                             <Input.TextArea placeholder="请输入" rows={10} />
                         </Form.Item>
                     </Form>
@@ -347,16 +351,16 @@ const Index = () => {
                 form.resetFields()
                 setOpen(true)
             }}>新增</Button>
-            <div style={{height: 12}}></div>           
-            <Table 
+            <div style={{ height: 12 }}></div>
+            <Table
                 scroll={{
                     x: "100vw",
                     y: `calc(100vh - 280px - ${document.getElementsByClassName("ant-table-thead")[0]?.clientHeight}px)`
                 }}
-                rowKey={`id`} 
-                columns={columns} 
-                loading={loading} 
-                dataSource={response || []} 
+                rowKey={`id`}
+                columns={columns}
+                loading={loading}
+                dataSource={response || []}
                 pagination={false} />
         </div>
     )
